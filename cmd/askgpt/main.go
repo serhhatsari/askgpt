9be9f4d8c2a6ff45b2c44a9d10a77baeff06dfa3
Serhat SARI <serhhatsari@gmail.com>
2023-09-16 03:11:21 +0300

    feat: add .env support

--- cmd/askgpt/main.go
+++ cmd/askgpt/main.go
@@ -1,30 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/serhhatsari/askgpt/internal/chat"
 	"github.com/serhhatsari/askgpt/internal/completions"
 	"github.com/serhhatsari/askgpt/internal/image"
 
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
+
+func welcomeMessage(cmd *cobra.Command, args []string) {
+	pterm.DefaultHeader.Println(pterm.Red("Welcome to AskGPT!"))
+	pterm.Println(pterm.Blue("AskGPT is a CLI to interact with OpenAI API\n"))
+}
 
 var cmdRoot = &cobra.Command{
 	Use:   "askgpt",
 	Short: "Simple CLI to interact with OpenAI API",
 	Long:  "Simple CLI to interact with OpenAI API and get answers to your questions or generate images.",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println()
-		pterm.DefaultHeader.Println(pterm.Red("Welcome to AskGPT!"))
-		pterm.Println(pterm.Blue("AskGPT is a CLI to interact with OpenAI API\n"))
-	},
+	Run:   welcomeMessage,
 }
 
 func main() {
-
-	cmdRoot.MarkPersistentFlagRequired("port")
 
 	cmdRoot.AddCommand(chat.CmdChat)
 	chat.CmdChat.Flags().Float32VarP(&chat.Temperature, "temperature", "t", 0, "Temperature of the model. Higher values will result in more creative completions, but also more likelihood of nonsensical text. Try 0, 0.5, 1.0, 1.5, 2.0")
@@ -35,16 +33,17 @@ func main() {
 	cmdRoot.AddCommand(image.CmdImage)
 	image.CmdImage.Flags().IntVarP(&image.Size, "size", "s", 256, "Size of the image to generate. Try 256, 512, 1024")
 
-	cmdRoot.Execute()
+	err := cmdRoot.Execute()
+	if err != nil {
+		pterm.Error.Println(err)
+		return
+	}
 }
 
 func init() {
-
-	viper.Set("Verbose", true)
-
-	viper.AutomaticEnv()
-
-	viper.SetConfigType("yaml")
-
-	viper.SetDefault("version", "0.0.1")
+	err := godotenv.Load()
+	if err != nil {
+		// Handle errors if the .env file cannot be loaded
+		panic("Error loading .env file")
+	}
 }
